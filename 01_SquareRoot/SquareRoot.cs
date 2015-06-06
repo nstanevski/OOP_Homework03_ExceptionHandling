c77bc5c094924a7973a7953babf00e5ae2b6bbdc
nstanevski <nikolay.stanevski@gmail.com>
2015-06-06 18:34:05 +0300

    task #1 and task#2 added

---
+++ 01_SquareRoot/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+ * Write a program that reads an integer number and calculates and prints 
+ * its square root. 
+ * If the number is invalid or negative, print "Invalid number". 
+ * In all cases finally print "Good bye". Use try-catch-finally.
+ */
+
+class SquareRoot
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        try
+        {
+            int n = int.Parse(str);
+            double d = Math.Sqrt((double)n);
+            if (double.IsNaN(d))
+                throw new ArgumentException();
+            Console.WriteLine("Number: {0} square root: {1:f8}", n, d);
+        }
+        catch (Exception eX)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ 02_EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class EnterNumbers
+{
+    private static void ReadNumber(int start, int end, out int number)
+    {
+        Console.WriteLine("Enter a number in range [{0} .. {1}]", start, end);
+        string str = Console.ReadLine();
+        try
+        {
+            number = int.Parse(str);
+        }
+        catch (Exception)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        if (number < start || number > end)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+
+    static void Main()
+    {
+        int start = 1;
+        int end = 100;
+        int count = 10;
+        bool successfulSequence = true;
+        int[] sequence = new int[10];
+        for(int i=0; i<count; i++){
+            //allow the user up to 3 tries to enter the number in valid range:
+            bool successfulEntry = false;
+            int numTries = 3;
+            while (!successfulEntry && numTries>0)
+            {
+                try
+                {
+                    int num = 0;
+                    ReadNumber(start + 1, end - count + i, out num);
+                    start = num;
+                    successfulEntry = true;
+                    sequence[i] = num;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    numTries--;
+                }
+            }
+            if (!successfulEntry && numTries == 0)
+            {
+                Console.WriteLine("Sorry, you failed after 3 tries to enter a number in specified range");
+                successfulSequence = false;
+                break;
+            }     
+        }
+        if (successfulSequence)
+            Console.WriteLine('['+string.Join(", ", sequence) + ']');
+    }
+}
